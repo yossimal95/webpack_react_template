5e9faa0985e19406615eda845dd93269dcf1a85f
yossimal95 <70268960+yossimal95@users.noreply.github.com>
2024-02-15 08:51:59 +0200

    Add files via upload

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Alert from "../../components/Alert/Alert";
+
+const Home = ({ }) => {
+    return (
+        <Alert text={"שלום שלום לדף-Home"}/>
+    );
+};
+
+export default Home;
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  output: {
+    path: path.join(__dirname, "/dist"), // the bundle output path
+    filename: "bundle.js", // the name of the bundle
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html", // to import index.html file inside index.js
+    }),
+  ],
+  devServer: {
+    //port: 3330, // you can change the port
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // .js and .jsx files
+        exclude: /node_modules/, // excluding the node_modules folder
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/, // styles files
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[local]--[hash:base64:5]',
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
+        loader: "url-loader",
+        options: { limit: false },
+      },
+    ],
+  },
+};
