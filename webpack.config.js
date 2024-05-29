3c95980fbc6f0a2c1105199c76e6905bc5cd3dd7
yossimal95 <70268960+yossimal95@users.noreply.github.com>
2024-05-29 20:49:48 +0300

    Adding LEVEL

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,73 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require('webpack')
 
-module.exports = {
-  output: {
-    path: path.join(__dirname, "/dist"), // the bundle output path
-    filename: "bundle.js", // the name of the bundle
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/index.html", // to import index.html file inside index.js
-    }),
-  ],
-  devtool: 'inline-source-map',
-  devServer: {
-    //port: 3330, // you can change the port
-  },
-  devServer: {
-    historyApiFallback: true,
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/, // .js and .jsx files
-        exclude: /node_modules/, // excluding the node_modules folder
-        use: {
-          loader: "babel-loader",
+module.exports = (env) => {
+  return {
+    output: {
+      path: path.join(__dirname, "/dist"), // the bundle output path
+      filename: "bundle.js", // the name of the bundle
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        LEVEL: JSON.stringify(env.LEVEL)
+      }),
+      new HtmlWebpackPlugin({
+        template: "src/index.html", // to import index.html file inside index.js
+      }),
+    ],
+    devtool: 'inline-source-map',
+    devServer: {
+      //port: 3330, // you can change the port
+    },
+    devServer: {
+      historyApiFallback: true,
+    },
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/, // .js and .jsx files
+          exclude: /node_modules/, // excluding the node_modules folder
+          use: {
+            loader: "babel-loader",
+          },
         },
-      },    
-      {
-        test: /.\.module\.css$/, // Match CSS files with .module.css extension
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: {
-                localIdentName: '[name]__[local]__[hash:base64:5]'
+        {
+          test: /.\.module\.css$/, // Match CSS files with .module.css extension
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                modules: {
+                  localIdentName: '[name]__[local]__[hash:base64:5]'
+                }
               }
             }
-          }
-        ]
-      },
-      {
-        test: /\.css$/, // Match regular CSS files
-        exclude: /\.module\.css$/, // Exclude CSS files with .module.css extension
-        use: ['style-loader', 'css-loader'] //, 
-      },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
-        loader: "url-loader",
-        options: { limit: false },
-      },
-      {
-        test: /\.(jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-          },
-        ],
-      },
-    ],
-  },
+          ]
+        },
+        {
+          test: /\.css$/, // Match regular CSS files
+          exclude: /\.module\.css$/, // Exclude CSS files with .module.css extension
+          use: ['style-loader', 'css-loader'] //, 
+        },
+        {
+          test: /\.(png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
+          loader: "url-loader",
+          options: { limit: false },
+        },
+        {
+          test: /\.(jpe?g|gif)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+            },
+          ],
+        },
+      ],
+    },
+  }
 };
